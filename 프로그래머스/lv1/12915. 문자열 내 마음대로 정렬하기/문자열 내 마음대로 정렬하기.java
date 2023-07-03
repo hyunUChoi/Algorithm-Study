b1de14e1bc069c1c86293cde4ac59486002aeb01
Hyunwoo Choi <chw64233@gmail.com>
2023-07-03 17:30:09 +0900

    Update 문자열 내 마음대로 정렬하기.java

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -15,3 +15,36 @@ class Solution {
         return answer;
     }
 }
+
+/**
+-- Map을 활용한 방법
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        Map<String, String> map = new HashMap<String, String>();
+        
+        for(String str : strings) {
+            -- n번째 글자를 Key로 사용하지 않는 이유 : 같은 알파벳인 경우 Iterator에 의해 삭제됨
+            map.put(str, str.charAt(n) + str);
+        }
+        
+        List<String> list = new ArrayList<>(map.values());
+        list.sort(String::compareTo);
+        
+        String[] answer = new String[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = getKey(map, list.get(i));
+        }
+        
+        return answer;
+    }
+    
+    public static <K, V> K getKey(Map<K, V> map, V value) {
+        for(K key : map.keySet()) {
+            if(value.equals(map.get(key))) {
+                return key;
+            }
+        }
+        return null;
+    }
+}
+**/
