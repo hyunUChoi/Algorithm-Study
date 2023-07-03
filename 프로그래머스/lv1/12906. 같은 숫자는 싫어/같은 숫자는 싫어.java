164aeda279314f04240ef9469b3b63bcaf671401
Hyunwoo Choi <chw64233@gmail.com>
2023-07-03 15:48:22 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 79.19 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,15 +1,38 @@
 import java.util.*;
 
 public class Solution {
-    public Stack<Integer> solution(int []arr) {
-        Stack<Integer> answer = new Stack<Integer>();
+    /*public int[] solution(int []arr) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(arr[0]);
         
-        for(int i : arr) {
-            if(answer.isEmpty() || answer.peek() != i) {
-                answer.push(i);
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] != arr[i - 1]) {
+                list.add(arr[i]);
             }
         }
+        
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
 
+        return answer;
+    }*/
+    
+    public int[] solution(int []arr) {
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        for(int i : arr) {
+            if(stack.isEmpty() || stack.peek() != i) {
+                stack.push(i);
+            }
+        }
+        
+        int[] answer = new int[stack.size()];
+        for(int i = stack.size() - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
+        }
+        
         return answer;
     }
 }
