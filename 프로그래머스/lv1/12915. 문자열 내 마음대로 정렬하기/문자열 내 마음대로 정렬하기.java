ad651a1b96e235194f31511b8d80e325a3f14dba
Hyunwoo Choi <chw64233@gmail.com>
2023-07-03 17:26:23 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 9.67 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -2,29 +2,16 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        Map<String, String> map = new HashMap<String, String>();
-        
-        for(String str : strings) {
-            map.put(str, str.charAt(n)+str);
+        for(int i = 0; i < strings.length; i++) {
+            // 앞에 인덱스 글자를 붙이고 뒤에 문자열 붙이기 -> 같은 알파벳이면 정렬 시 사전 순
+            strings[i] = strings[i].charAt(n) + strings[i];
         }
+        Arrays.sort(strings);
         
-        List<String> list = new ArrayList<>(map.values());
-        list.sort(String::compareTo);
-        
-        String[] answer = new String[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = getKey(map, list.get(i));
+        String[] answer = new String[strings.length];
+        for(int i = 0; i < strings.length; i++) {
+            answer[i] = strings[i].substring(1);
         }
-        
         return answer;
-    }
-    
-    public static <K, V> K getKey(Map<K, V> map, V value) {
-        for(K key : map.keySet()) {
-            if(value.equals(map.get(key))) {
-                return key;
-            }
-        }
-        return null;
     }
 }
