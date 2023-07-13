fea44aa9103c6a8a11f43b9a020b50e929eb2208
Hyunwoo Choi <chw64233@gmail.com>
2023-07-13 09:28:10 +0900

    Update 크기가 작은 부분문자열.java

--- 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -2,6 +2,8 @@ class Solution {
     public int solution(String t, String p) {
         int answer = 0;
         int pLength = p.length();
+        // int가 아닌 long으로 변환하는 이유
+        // -> int는 최대 10자리까지 long은 최대 19자리까지 가능
         long pToLong = Long.parseLong(p);
         
         for(int i = 0; i < t.length() - pLength + 1; i++) {
