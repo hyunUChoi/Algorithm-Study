efe6301bf1d9824d2c2607a79b9ffa0f35f2e4e8
Hyunwoo Choi <chw64233@gmail.com>
2023-07-14 09:46:33 +0900

    [level 1] Title: [1차] 비밀지도, Time: 6.58 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,4 +1,6 @@
 class Solution {
+    /* 
+    풀이 1
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         
@@ -30,5 +32,26 @@ class Solution {
         }
 
         return sb.reverse().toString();
+    }
+    */
+    
+    // 풀이2
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for(int i = 0; i < n; i++) {
+            // 2진수 변환 Integer.toBinaryString
+            // '|'을 통해 2진수 비교
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            
+            // String.format
+            // %s : 문자열형식
+            // %(number)s : 문자열 길이가 n보다 작을 경우 공백 추가
+            answer[i] = String.format("%" + n + "s", answer[i]);
+            answer[i] = answer[i].replaceAll("1" , "#");
+            answer[i] = answer[i].replaceAll("0" , " ");
+        }
+        
+        return answer;
     }
 }
