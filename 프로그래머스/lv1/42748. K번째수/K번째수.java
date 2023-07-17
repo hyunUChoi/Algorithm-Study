9cf9d3c826a5752379d4e28080f9159777a0242c
Hyunwoo Choi <chw64233@gmail.com>
2023-07-17 14:35:19 +0900

    [level 1] Title: K번째수, Time: 0.42 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.java
+++ 프로그래머스/lv1/42748. K번째수/K번째수.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 class Solution {
+    /*
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
@@ -10,7 +11,22 @@ class Solution {
                 list.add(array[j]);
             }
             Collections.sort(list);
-            answer[i] = list.size() == 1 ? list.get(0) : list.get(commands[i][2] - 1);
+            answer[i] = list.get(commands[i][2] - 1);
+        }
+        
+        return answer;
+    }
+    */
+    
+    // Arrays.copyOfRange 써보기
+    // Arrays.copyOfRange(복사할 원본배열, 복사 시작인덱스, 복사 끝인덱스)
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i = 0; i < commands.length; i++) {
+            int[] tmp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(tmp);
+            answer[i] = tmp[commands[i][2] - 1];
         }
         
         return answer;
