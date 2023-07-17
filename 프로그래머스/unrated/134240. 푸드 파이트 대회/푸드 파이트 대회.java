587bf95c0a956e726e2ce6fc0c48d001ad850d59
Hyunwoo Choi <chw64233@gmail.com>
2023-07-17 18:02:00 +0900

    Update 푸드 파이트 대회.java

--- 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,4 +1,22 @@
 class Solution {
+    /*
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 1; i < food.length; i++) {
+            for(int j = 0; j < food[i]/2; j++) {
+                answer += "" + i;
+                sb.append(i);
+            }
+        }
+
+        answer += "0" + sb.reverse().toString();
+
+        return answer;
+    }
+    */
+    
     public String solution(int[] food) {
         String answer = "0";
         
