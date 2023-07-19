7dcf5aaa992d0296e86ec6939c9f872827c43b3e
Hyunwoo Choi <chw64233@gmail.com>
2023-07-19 14:45:28 +0900

    [unrated] Title: 과일 장수, Time: 0.41 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 class Solution {
+    /*
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         int tmp = m - 1;
@@ -10,6 +11,17 @@ class Solution {
         while(tmp < score.length) {
             answer += arr[tmp] * m;
             tmp += m;
+        }
+        
+        return answer;
+    }
+    */
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Arrays.sort(score);
+        
+        for(int i = score.length - m; i >= 0; i -= m) {
+            answer += score[i] * m;
         }
         
         return answer;
